7b7db4ecf0f4b05adb3f97c097cbc6885bac6289
sonu2164 <sonu037singh@gmail.com>
2023-04-23 12:39:05 +0530

    file and folder structure

---
+++ src/components/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { CssBaseline } from '@mui/material';
+import { Routes, Route } from 'react-router-dom';
+
+import { Actors, MovieInformation, Movies, NavBar, Profile } from './index';
+import useStyles from './styles';
+
+function App() {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+
+      <CssBaseline />
+      <NavBar />
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+
+        <Routes>
+
+          <Route path="/movie/:id" element={<MovieInformation />} />
+          <Route path="/actors/:id" element={<Actors />} />
+          <Route path="/" element={<Movies />} />
+          <Route path="/profile/:id" element={<Profile />} />
+
+        </Routes>
+      </main>
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/components/MovieInformation/MovieInformation.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MovieInformation() {
+  return (
+    <div>
+      MovieInformation
+    </div>
+  );
+}
+
+export default MovieInformation;
